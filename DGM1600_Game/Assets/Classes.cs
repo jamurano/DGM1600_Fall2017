e1f2f4193e764de8fcfa63677a08a026d818c228
Unknown <jamurano@yahoo.com>
2017-08-30 20:41:23 -0600

    Added Unity files

---
+++ DGM1600_Game/Assets/Classes.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Classes : MonoBehaviour {
+
+	public class One {
+		public int money = 1000000;
+		public float fuel = 0.01f;
+
+		private string name = "Bob";
+
+	
+	}
+}
+public class Two : MonoBehaviour {
+
+		public int apples = 60;
+
+		public string greeting= "What's up?";
+
+		public float water = 12.5f;
+
+		
+}
+
+public class Three : MonoBehaviour {
+
+		public int health = 5;
+
+		public string plant = "Aspen";
+
+		public float oil = 2.25f;
+
+
+}
