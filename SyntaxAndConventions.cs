d9b8d5da1fce7156e4932163b456f20ca283c406
Jessica Murano <31229243+jamurano@users.noreply.github.com>
2017-09-11 20:54:15 -0600

    Add files via upload

---
+++ SyntaxAndConventions.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SyntaxAnd : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		public int papers = 100;
+		public string flower = "Dasiy";
+
+		public float waterBottle = .25f;
+
+		public string firstName = "Jessica";
+
+		public int newsPapers = 200;
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+// Syntax is the examples I did above. With float, you can do percentages, with string you can do phrases or things, and with int you can do whole numbers.
+// As far as I could research, a convention seems to be the entire code. I am probably wrong, but there wasn't any info about what a convention is.
