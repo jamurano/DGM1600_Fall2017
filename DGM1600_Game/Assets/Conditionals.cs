dd1f7d1036998c84e0b7f084324afb21ae68e87e
Unknown <jamurano@yahoo.com>
2017-09-11 20:14:17 -0600

    September

---
+++ DGM1600_Game/Assets/Conditionals.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conditionals : MonoBehaviour {
+
+public string stopLight = "Red";
+
+void Start(){
+	Main();
+}
+void Main(){
+
+	if(stopLight == "Red"){
+		print("Stop! The light is Red!");
+	}
+	else if(stopLight == "Yellow"){
+		print("Slow Down!");
+	}
+	else if(stopLight == "Green"){
+		print("Green means GO!");
+	}
+	else{
+		print("I Dont know what you mean!");
+	}
+}
+
+void Update () {
+		if(stopLight == "Red"){
+		print("Stop! The light is Red!");
+	}
+	else if(stopLight == "Yellow"){
+		print("Slow Down!");
+	}
+	else if(stopLight == "Green"){
+		print("Green means GO!");
+	}
+	else{
+		print("I Dont know what you mean!");
+	}
+}
+}
