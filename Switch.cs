6c17e384572724385773e89f73fa2651e9dfafae
Jessica Murano <31229243+jamurano@users.noreply.github.com>
2017-09-25 20:44:06 -0600

    Add files via upload

---
+++ Switch.cs
@@ -0,0 +1,187 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour {
+    public string townCenter = "main";
+
+	// Use this for initialization
+	void Start () {
+        switch (townCenter)
+        {
+            case "main":
+                print("Welcome to Main Street!");
+                break;
+            case "blacksmith":
+                print("The blacksmith grumbles as you pick through the sword bin");
+                break;
+            case "bakery":
+                print("Mmmmm... Baked goodness!");
+                break;
+            case "Morgue":
+                print("Peeeeeeeeeeyew! Dead things, yuck!");
+                break;
+            default:
+                print("I don't know what you are talking about!");
+                break;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+public class Switch2 : MonoBehaviour{
+    public string foodMart = "Food Mart";
+
+    void Start()
+    {
+        switch (foodMart)
+        {
+            case "Food Mart":
+                print("Welcome!");
+                break;
+            case "Apple":
+                print("Crunchy and juicy");
+                break;
+            case "Bread":
+                print("Avast Ye!! Sword fights with bread loafs.");
+                break;
+            default:
+                print("Get out of here!!");
+                break;
+        }
+   
+    }
+}
+public class Switch3 : MonoBehaviour
+{
+    public string plants = "Oooh, plants!";
+
+    void Start()
+    {
+        switch (plants)
+        {
+            case "Plants":
+                print("Some plants, party.");
+                break;
+            case "Cactus":
+                print("Ouch!!");
+                break;
+            case "Roses":
+                print("Pretty");
+                break;
+            default:
+                print("No more plants");
+                break;
+        }
+        
+    }
+
+}
+public class Switch4 : MonoBehaviour
+{
+    public string heros = "Good";
+
+    private void Start()
+    {
+        switch (heros)
+        {
+            case "Heros":
+                print("Choose your character!!"):
+                break;
+            case "Super Woman":
+                print("I'm Awesome and an Amazonian!");
+                break;
+            case "Batman":
+                print("I am the NIGHT!!");
+                break;
+            case "Aquaman":
+                print("I am lame");
+                break;
+            default:
+                print("Failure!!");
+                break;
+        }
+    }
+}
+public class Switch5 : MonoBehaviour
+{
+    public string villans = "Evil";
+    private void Start()
+    {
+        switch (villans)
+        {
+            case "Villans":
+                print("EEEEEEEEVVVVVVVIIIILLLL!!!!");
+                break;
+            case "Hades":
+                print("Boom baby!");
+                break;
+            case "Joker":
+                print("Why so serious?");
+                break;
+            default:
+                print("Have a penguin");
+                break;
+
+        }
+    }
+}
+public class Switch6 : MonoBehaviour
+{
+    public string colors = "Yay!";
+
+    private void Start()
+    {
+        switch (colors)
+        {
+            case "Colors":
+                print("There be colors");
+                break;
+            default:
+                print("Sometimes there be no colors");
+                break;
+        }
+    }
+}
+public class Switch7 : MonoBehaviour
+{
+    public string shoes = "Another object";
+
+    private void Start()
+    {
+        switch (shoes)
+        {
+            case "Heels":
+                print("Wow, it's like its a shoe");
+                break;
+            case "Tennis":
+                print("This is also a shoe");
+                break;
+            default:
+                print("surprise, i'm not a shoe");
+                break;
+        }
+    }
+}
+public class Switch8 : MonoBehaviour
+{
+    public string teeth = "I don't know";
+
+    private void Start()
+    {
+        case "Vampire":
+            print("They're sharp");
+            break;
+        case "Human":
+            print("Normal");
+             break;
+        default:
+            print("What are these!!?")
+            break;
+    }
+}
+// Switches are used to list different variables. The code when in use will go through each string in your code until it is true. 
---
+++ While.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class While : MonoBehaviour {
+    public int bottles = 99;
+
+	// Use this for initialization
+	void Start () {
+        while(bottles > 0)
+        {
+            print(bottles + "bottles of beer on the wall!");
+            bottles--;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+public class While2 : MonoBehaviour
+{
+    public int bottles = 0;
+
+    private void Start()
+    {
+        while(bottles < 100)
+        {
+            print(bottles + "bottles of beer on the wall!");
+            bottles++;
+        }
+    }
+
+    private void Update()
+    {
+
+    }
+}
+public class While3 : MonoBehaviour
+{
+    public int soap = 12;
+
+    private void Start()
+    {
+        while(soap > 0)
+        {
+            print(soap + "lots of soap");
+            soap--;
+        
+        }
+    }
+}
+public class While4 : MonoBehaviour
+{
+    public int candy = 0;
+
+    private void Start()
+    {
+        while(candy < 1000)
+        {
+            print(candy + "Yay! Candy!");
+            candy++;
+        }
+    }
+}
+public class While5 : MonoBehaviour
+{
+    public int candy = 1000;
+
+    private void Start()
+    {
+        while (candy > 0)
+        {
+            print(candy + "All the candy");
+            candy--;
+        }
+    }
+}
+public class While6 : MonoBehaviour
+{
+    public int soap = 0;
+
+    private void Start()
+    {
+        while(soap < 12)
+        {
+            print(soap + "Soap");
+            soap ++;
+        }
+    }
+}
+public class While7 : MonoBehaviour
+{
+    public int bugs = 5;
+
+    private void Start()
+    {
+        while(bugs > 0)
+        {
+            print(bugs + "Ewww!");
+            bugs++;
+        }
+    }
+}
+public class While8 : MonoBehaviour
+{
+    public int bugs = 0;
+
+    private void Start()
+    {
+        while(bugs < 5)
+        {
+            print(bugs + "Ewwwww");
+            bugs--;
+        }
+    }
+}
+// Whiles are used to be able to loop through numbers that you set with an int. They can count up or down until it hits the number you are seeking. But, loops can be dangerous because sometimes it can be infinite which will crash the system.
