1ba28e3dde4f5679b8c3407e6e3c49c109f80177
Jessica Murano <31229243+jamurano@users.noreply.github.com>
2017-10-09 19:40:42 -0600

    Add files via upload

---
+++ Functions.cs
@@ -0,0 +1,208 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Functions and parameters allow you to set a certain varible that the code wont change. It allows you to put in two numbers that it will add or do as instructed under a function.
+// Scope is the code in between the brackets.
+// Return allows the code to print the final code without you needing to type "print" and specifically writing what you want printed.
+public class Functions : MonoBehaviour {
+
+	public void DoStuff(){
+		print("I'm doing stuff...");
+	}
+	void Start(){
+	DoStuff();
+	int result = AddNumbers(12,4);
+	print(result);
+	}
+
+	public int AddNumbers(int num1, int num2){
+
+		int result = num1 + num2;
+
+		return result;
+	}
+
+
+}
+
+public class Functions2 : MonoBehaviour {
+
+	public void DoStuff(){
+		print("I'm doing stuff...");
+	}
+	void Start(){
+	DoStuff();
+	int result = AddNumbers(5,20);
+	print(result);
+	}
+
+	public int AddNumbers(int num1, int num2){
+
+		int result = num1 + num2;
+
+		return result;
+	}
+
+
+}
+public class Functions3 : MonoBehaviour {
+
+	public void DoStuff(){
+		print("I'm doing stuff...");
+	}
+	void Start(){
+	DoStuff();
+	int result = AddNumbers(7,100);
+	print(result);
+	}
+
+	public int AddNumbers(int num1, int num2){
+
+		int result = num1 + num2;
+
+		return result;
+	}
+
+
+}
+public class Functions4 : MonoBehaviour {
+
+	public void DoStuff(){
+		print("I'm doing stuff...");
+	}
+	void Start(){
+	DoStuff();
+	int result = AddNumbers(14,52);
+	print(result);
+	}
+
+	public int AddNumbers(int num1, int num2){
+
+		int result = num1 + num2;
+
+		return result;
+	}
+
+
+}
+public class Functions5 : MonoBehaviour {
+
+	public void DoStuff(){
+		print("I'm doing stuff...");
+	}
+	void Start(){
+	DoStuff();
+	int result = AddNumbers(5,11);
+	print(result);
+	}
+
+	public int AddNumbers(int num1, int num2){
+
+		int result = num1 + num2;
+
+		return result;
+	}
+
+
+}
+public class Functions6 : MonoBehaviour {
+
+	public void DoStuff(){
+		print("I'm doing stuff...");
+	}
+	void Start(){
+	DoStuff();
+	int result = AddNumbers(53,11);
+	print(result);
+	}
+
+	public int AddNumbers(int num1, int num2){
+
+		int result = num1 + num2;
+
+		return result;
+	}
+
+
+}
+public class Functions7 : MonoBehaviour {
+
+	public void DoStuff(){
+		print("I'm doing stuff...");
+	}
+	void Start(){
+	DoStuff();
+	int result = AddNumbers(400,11);
+	print(result);
+	}
+
+	public int AddNumbers(int num1, int num2){
+
+		int result = num1 + num2;
+
+		return result;
+	}
+
+
+}
+public class Functions8 : MonoBehaviour {
+
+	public void DoStuff(){
+		print("I'm doing stuff...");
+	}
+	void Start(){
+	DoStuff();
+	int result = AddNumbers(29,44);
+	print(result);
+	}
+
+	public int AddNumbers(int num1, int num2){
+
+		int result = num1 + num2;
+
+		return result;
+	}
+
+
+}
+public class Functions9 : MonoBehaviour {
+
+	public void DoStuff(){
+		print("I'm doing stuff...");
+	}
+	void Start(){
+	DoStuff();
+	int result = AddNumbers(5,11);
+	print(result);
+	}
+
+	public int AddNumbers(int num1, int num2){
+
+		int result = num1 + num2;
+
+		return result;
+	}
+
+
+}
+public class Functions10 : MonoBehaviour {
+
+	public void DoStuff(){
+		print("I'm doing stuff...");
+	}
+	void Start(){
+	DoStuff();
+	int result = AddNumbers(5,11);
+	print(result);
+	}
+
+	public int AddNumbers(int num1, int num2){
+
+		int result = num1 + num2;
+
+		return result;
+	}
+
+
+}
